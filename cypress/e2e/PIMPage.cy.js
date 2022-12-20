c62e17f3e475766cd9e5390e4b0b9e48637eb210
alif <alyfahmad@gmail.com>
2022-12-20 22:28:55 +0600

    fixed bugs in PIM

--- cypress/e2e/PIMPage.cy.js
+++ cypress/e2e/PIMPage.cy.js
@@ -1,5 +1,6 @@
 describe("verify create new employee and login info", () => {
   it("verify login and add new employee", () => {
+    cy.viewport(1920, 1080);
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
@@ -130,43 +131,83 @@ describe("verify create new employee and login info", () => {
     cy.xpath(
       "//button[contains(normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')), 'save')]"
     ).click();
+    // Validate previously inserted information
+    cy.xpath("//input[@placeholder='First Name']").should(
+      "have.value",
+      "tester f_name"
+    );
+    cy.xpath("//input[@placeholder='Middle Name']").should(
+      "have.value",
+      "tester m_name"
+    );
+    cy.xpath("//input[@placeholder='Last Name']").should(
+      "have.value",
+      "tester l_name"
+    );
+    //Add additional information
+    cy.xpath(
+      "//h6[normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='personal details']",
+      { timeout: 10000 }
+    ).should("be.visible");
+    cy.xpath(
+      "//label[normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='nickname']/parent::div/parent::div//input"
+    ).type("tester n_name");
+    cy.xpath(
+      "//label[normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='employee id']/parent::div/parent::div//input"
+    ).clear();
+    cy.xpath(
+      "//label[normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='employee id']/parent::div/parent::div//input"
+    ).type("999");
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'license number')]/parent::div/parent::div//input"
+    ).type("987-654-321");
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'license expiry date')]/parent::div/parent::div//input"
+    ).type("2025-12-01");
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'license expiry date')]/parent::div/parent::div//input"
+    ).click();
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'nationality')]/parent::div/parent::div//i"
+    ).click();
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'nationality')]/parent::div/parent::div//span[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'bangladeshi')]"
+    ).click();
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'marital status')]/parent::div/parent::div//i"
+    ).click();
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'marital status')]/parent::div/parent::div//span[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'single')]"
+    ).click();
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'date of birth')]/parent::div/parent::div//input"
+    ).type("2025-12-01");
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'date of birth')]/parent::div/parent::div//input"
+    ).click();
+    cy.xpath(
+      "//label[normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='male']/span"
+    ).click();
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'military service')]/parent::div/parent::div//input"
+    ).type("N/A");
+    cy.xpath(
+      "//label[normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='smoker']/parent::div/parent::div//i"
+    ).click();
+    cy.xpath(
+      "(//button[normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='save'])[1]"
+    ).click();
+
+    // add ssn & sin
+    cy.wait(5000);
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'ssn number')]/parent::div/parent::div//input"
+    ).type("987-654-321");
+    cy.xpath(
+      "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'sin number')]/parent::div/parent::div//input"
+    ).type("987-654-321");
+    cy.xpath(
+      "(//button[normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='save'])[1]"
+    ).click();
   });
-  // Validate previously inserted information
-  cy.xpath("//input[@placeholder='First Name']").should("eq", "tester f_name");
-  cy.xpath("//input[@placeholder='Middle Name']").type("eq", "tester m_name");
-  cy.xpath("//input[@placeholder='Last Name']").type("eq", "tester l_name");
-  //Add additional information
-  cy.xpath(
-    "//label[normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='nickname']/parent::div/parent::div//input"
-  ).type("tester n_name");
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'license number')]/parent::div/parent::div//input"
-  ).type("987-654-321");
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'license expiry date')]/parent::div/parent::div//input"
-  ).type("2025-12-01");
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'ssn number')]/parent::div/parent::div//input"
-  ).type("987-654-321");
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'sin number')]/parent::div/parent::div//input"
-  ).type("987-654-321");
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'nationality')]/parent::div/parent::div//input"
-  ).select("Bangladeshi");
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'marital status')]/parent::div/parent::div//input"
-  ).select("Single");
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'date of birth')]/parent::div/parent::div//input"
-  ).type("2025-12-01");
-  cy.xpath(
-    "//label[normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='male']/input"
-  ).check();
-  cy.xpath(
-    "//label[contains(normalize-space(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')),'military service')]/parent::div/parent::div//input"
-  ).type("N/A");
-  cy.xpath(
-    "//label[normalize-space(translate(.,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'))='smoker']/parent::div/parent::div//input"
-  ).check();
 });
